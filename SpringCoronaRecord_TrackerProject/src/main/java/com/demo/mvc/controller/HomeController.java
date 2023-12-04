04f74c452bb521201e51cf5bd08e065da11fd775
chithraep <epchithra910@gmail.com>
2023-12-04 13:35:15 +0530

    this is my REST Api corona tracker project

---
+++ SpringCoronaRecord_TrackerProject/src/main/java/com/demo/mvc/controller/HomeController.java
@@ -0,0 +1,29 @@
+package com.demo.mvc.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.demo.mvc.model.LocationStates;
+import com.demo.mvc.services.CoronaVirusDataServices;
+
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	CoronaVirusDataServices crnService;
+	@GetMapping("/")
+	public String home(Model model)
+	{
+		List<LocationStates> allstates=crnService.getAllstates();
+		int totalDeathsReported=allstates.stream().mapToInt(stat->stat.getLatestTotalDeaths()).sum();
+		model.addAttribute("LocationStates",allstates);
+		model.addAttribute("totalDeathsReported",totalDeathsReported);
+		return "home";
+	}
+
+}
